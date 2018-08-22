d3a2a5b2deb3e90dc66f7cd3ecd1d1c47a18ae48
Pim <pimgroeneveld92@gmail.com>
2018-08-22 11:12:12 +0100

    mvp + notes extensions

---
+++ specs/rat_spec.js
@@ -0,0 +1,15 @@
+// const Rat = require("../rat");
+// const assert = require("assert");
+//
+// describe("Rat", function(){
+//
+//
+// })
+// look into homework solution
+
+// when rat touches food --> food becomes poisonous
+// either make food value negtive, or create status check (true or false)
+// set food value to false standard, just create a .touchfood function that marks the food as true for poisonous --> keep it simple!
+
+// following in hero class, create test to see if health goes down when eats poisonous food. require in both rat and food into file
+// adjust eatFood function, at top of if loop, set condition and have hero decrease health value. --> change if favFood to else if so it doesnt run when firt if (poison) is correct
